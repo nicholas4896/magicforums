3aa6df78fa1a14981051677993e5eef0ef44a504
Nicholas <nicholasyhs@gmail.com>
2016-08-03 17:50:54 +0800

    fix link_to, update content, topics css

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,22 @@
+class TopicsController < ApplicationController
+
+  def index
+    @topics = Topic.all 
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
