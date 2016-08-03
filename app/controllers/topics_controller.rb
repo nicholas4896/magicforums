c77ebcca0197dc5db755f154faa3841e65d9b06e
Nicholas <nicholasyhs@gmail.com>
2016-08-04 06:20:54 +0800

    'working'

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,22 +1,56 @@
 class TopicsController < ApplicationController
 
   def index
-    @topics = Topic.all 
+    @topics = Topic.all.order(created_at: :desc)
+    @topic =Topic.new
+
   end
 
   def show
+    @topic = Topic.find_by(id: params[:id])
+  end
+
+  def new
+    @topic = Topic.new  
   end
 
   def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      redirect_to topics_path
+    else
+      render new_topic_path
+    end
   end
 
   def edit
+    @topic = Topic.find_by(id: params[:id])
   end
 
   def update
+    @topic = Topic.find_by(id: params[:id])
+
+    if @topic.update(topic_params)
+      redirect_to topic_path(@topic)
+    else
+      redirect_to edit_topic_path(@topic)
+    end
   end
 
   def destroy
+    @topic = Topic.find_by(id: params[:id])
+    if @topic.destroy
+      redirect_to topics_path
+    else
+      redirect_to topics_path(@topic)
+    end
+  end
+
+private
+
+  def topic_params
+    params.require(:topic).permit(:title, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
   root to: 'landing#index'
+
 get :about, to: 'static_pages#about'
+
 resources :topics
+
+get :topics, to: 'topics#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
